050333093343f68c87622c2dbbf06d6e0bfe9ad2
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2024-07-02 23:35:41 +0700

    Make it work for electron

---
+++ forge.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+    packagerConfig: {
+        icon: "./icons/appicon",
+    makers: [
+        {
+            name: "@electron-forge/maker-squirrel",
+            config: {
+                name: "Blob Opera",
+                setupIcon: "./icons/appicon.ico", // Specific for Squirrel.Windows
+            },
+        },
+        {
+            name: "@electron-forge/maker-zip",
+            platforms: ["darwin"],
+        },
+        {
+            name: "@electron-forge/maker-dmg",
+            config: {
+                icon: "./icons/appicon-macos.icns", // macOS specific icon
+            },
+        },
+        {
+            name: "@electron-forge/maker-deb",
+            config: {
+                icon: "./icons/appicon-linux.png", // Linux specific icon for deb
+            },
+        },
+        {
+            name: "@electron-forge/maker-rpm",
+            config: {
+                icon: "./icons/appicon-linux.png", // Linux specific icon for rpm
+            },
+        },
+    ],
+};
